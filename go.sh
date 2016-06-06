f11e95cc14fa6cadd524881a691eb205907272b7
Jez Humble <jez@jezhumble.net>
2016-06-06 13:18:25 -0700

    We now set terraform remote state

--- go.sh
+++ go.sh
@@ -6,12 +6,14 @@ die () {
 
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 
-terraform remote config -backend=s3 -backend-config="bucket=tts_prod_terraform_state" -backend-config="key=network/terraform.tfstate" -backend-config="region=us-east-1"
+TERRAFORM_CONFIG=terraform remote config -backend=s3 -backend-config="bucket==${ENVIRONMENT_NAME}_terraform_state" -backend-config="key=network/terraform.tfstate" -backend-config="region=us-east-1"
 
 ROOT_DIR=`pwd`
 ENVIRONMENT_NAME=$1
 TIMESTAMP=`date +"%Y-%m-%d_%H-%M-%S"`
 echo "Building image at time ${TIMESTAMP}"
+cd $ROOT_DIR/terraform/packer
+eval $TERRAFORM_CONFIG
 terraform apply -var env_name=${ENVIRONMENT_NAME} $ROOT_DIR/terraform/packer
 PACKER_SUBNET=`terraform output packer_subnet`
 PACKER_VPC=`terraform output packer_vpc`
@@ -21,4 +23,5 @@ packer build -machine-readable -var "vpc_id=$PACKER_VPC" -var "subnet_id=$PACKER
 AMI_NAME=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
 echo $AMI_NAME
 cd $ROOT_DIR/terraform/concourse
+eval $TERRAFORM_CONFIG
 terraform apply -var ami_name=$AMI_NAME $ROOT_DIR/terraform/concourse
