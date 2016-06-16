18688c7849ebde2d5e733049e3ba7f007c517b57
Jez Humble <jez@jezhumble.net>
2016-06-15 20:48:34 -0700

    We now only build images if none exist already

--- go.sh
+++ go.sh
@@ -8,6 +8,7 @@ die () {
 config_s3_terraform()
 {
     aws s3 mb s3://$1_$2_terraform_state
+    cd $ROOT_DIR/terraform/$2
     terraform remote config -backend=s3 -backend-config="bucket=${1}_${2}_terraform_state" -backend-config="key=network/terraform.tfstate" -backend-config="region=us-east-1"
 }
 
@@ -18,28 +19,26 @@ fi
 
 #Set up some variables
 ROOT_DIR=`pwd`
-TIMESTAMP=`date +"%Y-%m-%d_%H-%M-%S"`
 
 #Terraform environment for packer to use to create AMI for Concourse box
-echo "Building image at time ${TIMESTAMP}"
-cd $ROOT_DIR/terraform/packer
 config_s3_terraform $ENVIRONMENT_NAME "packer"
 terraform apply -var env_name=${ENVIRONMENT_NAME} $ROOT_DIR/terraform/packer
 PACKER_SUBNET=`terraform output packer_subnet`
 PACKER_VPC=`terraform output packer_vpc`
 
 #Create AMI for Concourse box
-export LC_CTYPE=C
-GOCD_PASSWORD=$(tr -cd "[:alnum:]" < /dev/urandom | fold -w30 | head -n1)
-echo "Credentials for Go: username raktabija, password ${GOCD_PASSWORD}"
-cd $ROOT_DIR/packer
-packer build -machine-readable -var "vpc_id=${PACKER_VPC}" -var "subnet_id=${PACKER_SUBNET}" -var "ami_name_postfix=${TIMESTAMP}" -var "env_name=${ENVIRONMENT_NAME}" -var "gocd_password=${GOCD_PASSWORD}" bootstrap_concourse.json | tee build.log
-[[ ${PIPESTATUS[0]} -eq 0 ]] || die "Packer failed, exiting"
-AMI_NAME=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
+AMI_NAME=`aws ec2 describe-images --owners self --filters Name=tag:Environment,Values=${ENVIRONMENT_NAME} Name=tag:Creator,Values=packer --query 'Images[*].{DATE:CreationDate,ID:ImageId}' --output text | sort -r | cut -f 2 | head -n1`
+if [ -z ${AMI_NAME+x} ]; then
+    export LC_CTYPE=C
+    GOCD_PASSWORD=$(tr -cd "[:alnum:]" < /dev/urandom | fold -w30 | head -n1)
+    echo "Credentials for Go: username raktabija, password ${GOCD_PASSWORD}"
+    cd $ROOT_DIR/packer
+    packer build -machine-readable -var "vpc_id=${PACKER_VPC}" -var "subnet_id=${PACKER_SUBNET}" -var "env_name=${ENVIRONMENT_NAME}" -var "gocd_password=${GOCD_PASSWORD}" bootstrap_concourse.json | tee build.log
+    [[ ${PIPESTATUS[0]} -eq 0 ]] || die "Packer failed, exiting"
+    AMI_NAME=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
+fi
 echo $AMI_NAME
 
 #Create Concourse environment in AWS
-cd $ROOT_DIR/terraform/concourse
-APPNAME=gocd
 config_s3_terraform $ENVIRONMENT_NAME "gocd"
-terraform apply -var "ami_name=$AMI_NAME" -var "env_name=${ENVIRONMENT_NAME}" $ROOT_DIR/terraform/concourse
+terraform apply -var "ami_name=$AMI_NAME" -var "env_name=${ENVIRONMENT_NAME}" $ROOT_DIR/terraform/gocd
--- terraform/gocd/initialize.tf
+++ terraform/gocd/initialize.tf
@@ -10,7 +10,7 @@ resource "aws_key_pair" "raktabija" {
   public_key = "${var.public_key}"
 }
 
-resource "aws_vpc" "concourse" {
+resource "aws_vpc" "gocd" {
     cidr_block = "10.0.0.0/24"
     tags {
     	 Creator = "Terraform"
@@ -25,8 +25,8 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
     }
 }
 
-resource "aws_subnet" "concourse_subnet_1" {
-    vpc_id = "${aws_vpc.concourse.id}"
+resource "aws_subnet" "gocd_subnet_1" {
+    vpc_id = "${aws_vpc.gocd.id}"
     availability_zone = "us-east-1a"
     cidr_block = "10.0.0.0/25"
     map_public_ip_on_launch = false
@@ -35,8 +35,8 @@ resource "aws_subnet" "concourse_subnet_1" {
     }
 }
 
-resource "aws_subnet" "concourse_subnet_2" {
-    vpc_id = "${aws_vpc.concourse.id}"
+resource "aws_subnet" "gocd_subnet_2" {
+    vpc_id = "${aws_vpc.gocd.id}"
     availability_zone = "us-east-1c"
     cidr_block = "10.0.0.128/25"
     map_public_ip_on_launch = false
@@ -79,16 +79,16 @@ resource "aws_iam_role_policy" "power_user_policy" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "concourse_profile" {
-    name = "concourse_profile"
+resource "aws_iam_instance_profile" "gocd_profile" {
+    name = "gocd_profile"
     roles = ["${aws_iam_role.ec2_role.name}"]
 }
 
-resource "aws_launch_configuration" "concourse_autoscale_conf" {
+resource "aws_launch_configuration" "gocd_autoscale_conf" {
     image_id = "${var.ami_name}"
     key_name = "${aws_key_pair.raktabija.id}"
     instance_type = "t2.medium"
-    iam_instance_profile = "${aws_iam_instance_profile.concourse_profile.arn}"
+    iam_instance_profile = "${aws_iam_instance_profile.gocd_profile.arn}"
     security_groups = ["${aws_security_group.allow_bastion.id}"]
     associate_public_ip_address = true
     lifecycle {
@@ -99,38 +99,38 @@ env_name: ${var.env_name}
 EOF
 }
 
-resource "aws_internet_gateway" "concourse_gw" {
-  vpc_id = "${aws_vpc.concourse.id}"
+resource "aws_internet_gateway" "gocd_gw" {
+  vpc_id = "${aws_vpc.gocd.id}"
   tags {
     Creator = "Terraform"
   }
 }
 
-resource "aws_route_table" "concourse_route_table" {
-    vpc_id = "${aws_vpc.concourse.id}"
+resource "aws_route_table" "gocd_route_table" {
+    vpc_id = "${aws_vpc.gocd.id}"
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.concourse_gw.id}"
+        gateway_id = "${aws_internet_gateway.gocd_gw.id}"
     }
     tags {
     	 Creator = "Terraform"
     }
 }
 
-resource "aws_route_table_association" "concourse_route_subnet_1" {
-    subnet_id = "${aws_subnet.concourse_subnet_1.id}"
-    route_table_id = "${aws_route_table.concourse_route_table.id}"
+resource "aws_route_table_association" "gocd_route_subnet_1" {
+    subnet_id = "${aws_subnet.gocd_subnet_1.id}"
+    route_table_id = "${aws_route_table.gocd_route_table.id}"
 }
 
-resource "aws_route_table_association" "concourse_route_subnet_2" {
-    subnet_id = "${aws_subnet.concourse_subnet_2.id}"
-    route_table_id = "${aws_route_table.concourse_route_table.id}"
+resource "aws_route_table_association" "gocd_route_subnet_2" {
+    subnet_id = "${aws_subnet.gocd_subnet_2.id}"
+    route_table_id = "${aws_route_table.gocd_route_table.id}"
 }
 
 resource "aws_security_group" "allow_bastion" {
   name = "allow_bastion"
   description = "Allow inbound ssh, ping and http"
-  vpc_id = "${aws_vpc.concourse.id}"
+  vpc_id = "${aws_vpc.gocd.id}"
 
   ingress {
     from_port = 80
@@ -162,10 +162,10 @@ resource "aws_security_group" "allow_bastion" {
   }
 }
 
-resource "aws_elb" "concourse_elb" {
-# depends_on = ["${aws_internet_gateway.concourse_gw.id}"]
-  name = "terraform-concourse-elb"
-  subnets = ["${aws_subnet.concourse_subnet_1.id}", "${aws_subnet.concourse_subnet_2.id}"]
+resource "aws_elb" "gocd_elb" {
+# depends_on = ["${aws_internet_gateway.gocd_gw.id}"]
+  name = "terraform-gocd-elb"
+  subnets = ["${aws_subnet.gocd_subnet_1.id}", "${aws_subnet.gocd_subnet_2.id}"]
   security_groups = ["${aws_security_group.allow_bastion.id}"]
 
   listener {
@@ -192,16 +192,16 @@ resource "aws_elb" "concourse_elb" {
   }
 }
 
-resource "aws_autoscaling_group" "concourse_autoscale" {
+resource "aws_autoscaling_group" "gocd_autoscale" {
   availability_zones = ["us-east-1a", "us-east-1c"]
   max_size = 1
   min_size = 1
   health_check_grace_period = 300
   health_check_type = "EC2"
   desired_capacity = 1
-  vpc_zone_identifier = ["${aws_subnet.concourse_subnet_1.id}", "${aws_subnet.concourse_subnet_2.id}"]
-  launch_configuration = "${aws_launch_configuration.concourse_autoscale_conf.id}"
-  load_balancers = ["${aws_elb.concourse_elb.id}"]
+  vpc_zone_identifier = ["${aws_subnet.gocd_subnet_1.id}", "${aws_subnet.gocd_subnet_2.id}"]
+  launch_configuration = "${aws_launch_configuration.gocd_autoscale_conf.id}"
+  load_balancers = ["${aws_elb.gocd_elb.id}"]
   tag {
     key = "Creator"
     value = "Terraform"
