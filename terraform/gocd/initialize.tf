739d42b6e73cbbea3b2176cb99f73b5936901b17
Jez Humble <jez@jezhumble.net>
2016-06-17 22:22:09 -0700

    Let the TCP out

--- terraform/gocd/initialize.tf
+++ terraform/gocd/initialize.tf
@@ -189,6 +189,13 @@ resource "aws_security_group" "allow_bastion" {
       cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     	 Creator = "Terraform"
   }
