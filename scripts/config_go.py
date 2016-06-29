2eba94f6d2e07f113fee8eff43cb7f80c01c66ac
Jez Humble <jez@jezhumble.net>
2016-06-28 18:10:18 -0700

    Bugfix

--- scripts/config_go.py
+++ scripts/config_go.py
@@ -7,9 +7,22 @@ parser = argparse.ArgumentParser(description='Configure go.')
 parser.add_argument('config', help="Go's config file")
 args = parser.parse_args()
 
+# get server id from existing config file
+tree = et.parse(args.config)
+root = tree.getroot()
+server = root.findall('server')[0]
+serverId = server.get('serverId')
+
+# get template
 tree = et.parse('packer/cookbooks/gocd/templates/cruise-config.xml.erb')
 root = tree.getroot()
 pipelines = root.findall('pipelines')[0]
+
+# set server id
+server = root.findall('server')[0]
+server.set('serverId', serverId)
+
+# add new pipeline
 pipeline = et.SubElement(pipelines, 'pipeline', {'name':'kali'})
 timer = et.SubElement(pipeline, 'timer', {'onlyOnChanges':'false'})
 timer.text = '0 0 22 ? * *'
@@ -18,7 +31,10 @@ git = et.SubElement(materials, 'git', {'url':'https://github.com/18F/raktabija.g
 vcs_filter = et.SubElement(git, 'filter')
 et.SubElement(vcs_filter, 'ignore', {'pattern':'**'})
 stage = et.SubElement(pipeline,'stage', {'name':'bootstrap'})
-job = et.SubElement(stage,'job', {'name':'exec'})
+jobs = et.SubElement(stage,'jobs')
+job = et.SubElement(jobs,'job', {'name':'exec'})
 tasks = et.SubElement(job,'tasks')
 et.SubElement(tasks, 'exec', {'args':'scripts/kali_cron', 'command':'/bin/bash'})
+
+# write out config file
 tree.write(args.config)
